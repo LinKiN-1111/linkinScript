f2ab39104c955d06bc06983ad6f54a6ba35a3b4b
linkin <linkin5epk@gmail.com>
2025-08-18 14:47:56 +0800

    fix some bug

--- script/lk.py
+++ script/lk.py
@@ -3,7 +3,7 @@
 import os
 script_dir = os.path.dirname(os.path.abspath(__file__))  # lkcmd.py 的位置
 root_dir = os.path.dirname(script_dir)                   # 退到 MYSCRIPT/
-file_path = os.path.join(root_dir, "data", "lkcmd.txt")  # 拼接目标路径
+file_path = os.path.join(root_dir, "data", "lk.txt")  # 拼接目标路径
 
 with open(file_path, "r",encoding='utf-8') as file:
     print("".join(file.readlines()))
